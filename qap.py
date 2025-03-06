78f1186af5cd05029a36143cc28fc8ff4ee6e58e
herc <101748155+mouxmoux@users.noreply.github.com>
2025-03-06 18:43:18 +0200

    converted to package for use with pandas dataframes

--- qap.py
+++ qap.py
@@ -27,42 +27,28 @@ def style_axes(subplot_n, ax, data, y_axis):
 
 
 def create_subplot_ratios(data):
-    max_tens = [math.ceil(max(arr) / 10.0) * 10 for arr in data]
+    max_tens = [math.ceil(max(col) / 10.0) * 10 for _, col in data.items()]
     return [x / sum(max_tens) for x in max_tens]
 
 
-################################################################################
-# Example data
-input_data = [[10, 20, 30, 50, 60, 80, 90, 110, 150, 170, 210, 250],
-              [11, 8, 21, 10, 35, 57, 19, 32, 0, 14, 26, 48], 
-              [4, 23, 10, 41, 34, 3, 20, 21, 84, 53, 15, 11], 
-              [5.3, 30, 0, 11, 35, 43, 10, 1, 4.3, 23, 32, 11],
-              [4, 3, 0, 1, 4, 3, 0, 1, 4, 3, 5, 1]]
-input_data_dict = { # Should eventually read dicts to pull sp names
-    "height": [10, 20, 30, 50, 60, 80, 90, 110, 150, 170, 210, 250],
-    "Sp1": [1, 8, 1, 10, 5, 7, 9, 2, 0, 4, 6, 8],
-    "Sp2": [4, 3, 0, 1, 4, 3, 0, 1, 4, 3, 4, 1]
-}
-
-height_column = 0 # TODO In given excel/csv, height column is assumed 1st
-
-
-################################################################################
 # Quantitative abundance plots
-heights = input_data.pop(height_column)
-fig, axes = plt.subplots(ncols=len(input_data),
-                         sharey=True,
-                         figsize=(8, 6),
-                         gridspec_kw={'width_ratios': create_subplot_ratios(input_data)})
+def qap_build(input_data):
+    fig, axes = plt.subplots(
+        ncols=len(list(input_data)),
+        sharey=True,
+        figsize=(1 * len(list(input_data)) + 2, 8),
+        gridspec_kw={'width_ratios': create_subplot_ratios(input_data)}
+    )
+    for i in range(len(list(input_data))):
+        species_name = input_data.iloc[:, i].name
+        abundances = input_data.iloc[:, i].values
+        heights = input_data.index.values
+        ax = axes[i]
+        ax.plot(abundances, heights, marker='.', linestyle='-', linewidth=0, color='black')
+        ax.fill_betweenx(heights, abundances, color='#206599')
+        ax.set_xlabel('%')
+        ax.set_title(species_name, y=1.1, rotation = 85)
+        style_axes(i, ax, abundances, heights)
 
-for i in range(len(input_data)):
-    arr = np.array(input_data[i])
-    ax = axes[i]
-    ax.plot(arr, heights, marker='.', linestyle='-', linewidth=0, color='black')
-    ax.fill_betweenx(heights, arr, color='#206599')
-    ax.set_xlabel('%')
-    ax.set_title('sp')
-    style_axes(i, ax, arr, heights)
-
-plt.tight_layout()
-plt.show()
+    fig.tight_layout()
+    return fig
